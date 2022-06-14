e2c1851e3007b6b158cf1d80ead7fa85d7a5962d
Oleg S home <tipkor@mail.ru>
2022-06-14 22:48:44 +0300

    tests, css, img

--- polls/admin.py
+++ polls/admin.py
@@ -13,8 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ] 
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
 
