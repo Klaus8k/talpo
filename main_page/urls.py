5c0b7fc053321eff2e6a0ce839b0bee804851093
Oleg Sokolov <klaus888@yandex.ru>
2022-06-16 14:41:46 +0300

    work template main

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.shortcuts import render, HttpResponse
 from . import views
 
+app_name = 'main_page'
 urlpatterns = [
-    path('', views.index, name='main_index')
+    path('', views.main_index, name='main_index')
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+from django.template import loader
 # Create your views here.
 def main_index(request):
    print('--------------', request, '--------------')
 
-   responce = render(request, template_name='main_page/index.html')
-   return responce
+   template = loader.get_template('main_page/index.html')
+#   responce = render(request, template_name='main_page/index.html')
+   return HttpResponse(template.render(request=request, context={}))
