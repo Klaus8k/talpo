2933f9f4f73d86070323084b885b2ba6a3862566
Oleg Sokolov <klaus888@yandex.ru>
2022-06-16 15:05:32 +0300

    template with context

--- main_page/views.py
+++ main_page/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
-from django.template import loader
-# Create your views here.
-def main_index(request):
-   print('--------------', request, '--------------')
 
-   template = loader.get_template('main_page/index.html')
-#   responce = render(request, template_name='main_page/index.html')
-   return HttpResponse(template.render(request=request, context={}))
+
+# Create your views here.
+
+def main_index(request):
+   context = {'site_name': 'talpo.ru'}
+   response = render(request, template_name='main_page/index.html',
+                     context=context)
+
+   return response
