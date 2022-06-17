a9b4319885ba7c3634799de0e881e1d2e9e378c7
Oleg Sokolov <klaus888@yandex.ru>
2022-06-17 15:34:34 +0300

    branch -d main_page

--- main_page/views.py
+++ main_page/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
+
+
+
+
+
 def main_index(request):
-   context = {'site_name': 'talpo.ru'}
+   context = {'site_name': 'talpo.ru', 'current_name': 'Oleg'}
    response = render(request, template_name='main_page/index.html',
                      context=context)
 
