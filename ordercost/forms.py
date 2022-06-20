7065b802c7958fd4ca066fc841ab454144c103d2
Oleg S home <tipkor@mail.ru>
2022-06-20 23:27:09 +0300

    add forms to offset.html

--- ordercost/forms.py
+++ ordercost/forms.py
@@ -5,4 +5,6 @@ class OffsetForms(forms.Form):
     circulation = forms.IntegerField(min_value=1)
     size_x = forms.IntegerField(min_value=25)
     size_y = forms.IntegerField(min_value=25)
-    weith_paper = forms.ChoiceField(choices = [(130,'130'),(170,'170'),(300,'300')])
+    weith_paper = forms.ChoiceField(choices = [(130,'130'),
+                                               (170,'170'),
+                                               (300,'300')])
