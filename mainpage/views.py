0fd5df7d2bd098350ec96315410ba9463ab5b579
Oleg Sokolov <klaus888@yandex.ru>
2022-06-20 16:54:27 +0300

    меню с ссылками

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
+
+from talpo import settings
 from .forms import MathForm
 
 # Create your views here.
 
-PAGES = ['']
+PAGES = settings.INSTALLED_APPS[6:] # non django apps. better do it with split('.') no django
+
 
 def index(request):
 
-    return render(request, 'mainpage/index.html', {'title': 'mainpage'})
+        return render(request, 'mainpage/index.html', {'apps': PAGES})
 
