d193a8c7e1ac1d8cce8b9ea15459796d55317df5
Oleg S home <tipkor@mail.ru>
2022-07-03 00:20:52 +0300

    + немного причесанное меню и включение его в шаблоны приложения

--- ordercost/forms.py
+++ ordercost/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 
-
-PAPER = [(130,'130'), # Переменную берем из модуля бизнеслогики
-        (170,'170'),
-        (300,'300')
-         ]
+PAPER = [(130, '130'),  # Переменную берем из модуля бизнеслогики
+         (170, '170'),
+         (300, '300')]
 
 
 class OffsetForms(forms.Form):
-    circulation = forms.IntegerField(min_value=1)
+    calculation = forms.IntegerField(min_value=1)
     size_x = forms.IntegerField(min_value=25)
     size_y = forms.IntegerField(min_value=25)
-    weith_paper = forms.Select(choices = PAPER)
+    weigth_paper = forms.ChoiceField(choices=PAPER)
--- ordercost/views.py
+++ ordercost/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
+
 from .forms import OffsetForms
 from .logic import result
 
@@ -17,9 +18,10 @@ def index(request):
 def offset(request):
     context = {'unit': 'offset'}
 
-    if request.GET.get('reset'): # при ключе ресет переходит на реквест но только путь path
+    if request.GET.get('reset'):  # при ключе ресет переходит на реквест но только путь path
         return HttpResponseRedirect(request.path)
-# если есть данные из формы передает очищенные данные в функцию обработки
+
+    # если есть данные из формы передает очищенные данные в функцию обработки
     if request.method == 'GET':
         form = OffsetForms(request.GET)
         if form.is_valid():
